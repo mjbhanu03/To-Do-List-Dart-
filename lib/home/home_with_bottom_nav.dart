aa80e066b84374e9f5cc25d1394ceff7655afa01
Mange Jay <jaymange263@gmail.com>
2025-04-12 19:53:39 +0530

    'viewsadded'

--- lib/home/home_with_bottom_nav.dart
+++ lib/home/home_with_bottom_nav.dart
@@ -7,13 +7,30 @@ class HomeWithBottomNav extends StatefulWidget{
 
 class _HomeWithBottomNav extends State<HomeWithBottomNav>{
   int _selectedIndex = 0;
-  
+
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text('Display Page'),),
+    Center(child: Text('Add Page'),)
+  ];
+
+  void onItemTapped(int index){
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: Text('Home with Bottom Nav'),),
-      body: Center(
-        child: Text('Welcome to Page 2'),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: onItemTapped,
+          items: const [
+            BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Display'),
+            BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add')
+          ]
       ),
     );
   }
--- lib/home/home_with_nav_drawer.dart
+++ lib/home/home_with_nav_drawer.dart
@@ -1,17 +1,59 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class HomeWithNavDrawer extends StatefulWidget{
+class HomeWithNavDrawer extends StatefulWidget {
+  @override
   _HomeWithNavDrawer createState() => _HomeWithNavDrawer();
 }
 
-class _HomeWithNavDrawer extends State<HomeWithNavDrawer>{
+class _HomeWithNavDrawer extends State<HomeWithNavDrawer> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    Center(child: Text('Display Page')),
+    Center(child: Text('Add Page')),
+  ];
+
+  void onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Home with Nav Drawer'),),
-      body: Center(
-        child: Text('Welcome to page 3'),
+      appBar: AppBar(title: Text('Home with Nav Drawer')),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            DrawerHeader(child: Text('Menu')),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context); // only close drawer here
+                onItemTapped(0);
+              },
+              title: Text('Display'),
+            ),
+            ListTile(
+              onTap: () {
+                Navigator.pop(context);
+                onItemTapped(1);
+              },
+              title: Text('Add'),
+            ),
+          ],
+        ),
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: onItemTapped,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home_max), label: 'Display'),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Add'),
+        ],
       ),
     );
   }
